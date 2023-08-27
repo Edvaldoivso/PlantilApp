beadfde66c39e3d773340a40739a9e4169885fc2
Edvaldo <49847797+Edvaldoivso@users.noreply.github.com>
2023-08-27 18:12:38 -0300

    Criada a estrutura de Login e da Home

---
+++ src/app/authentication/authentication.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: './authentication.component.html',
+  styleUrls: ['./authentication.component.scss']
+})
+export class AuthenticationComponent implements OnInit {
+
+
+  public constructor(private router: Router) { }
+
+
+  ngOnInit(): void {
+
+    console.log('iniciado auth');
+   
+  }
+
+  public redirect() {
+    this.router.navigate(['home']);
+  }
+
+
+
+
+}
